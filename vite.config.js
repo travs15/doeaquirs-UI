8f56d9138d9a560f0f064cae63176af63ac463b8
Alejandro Reyes <alejo.reyes.p15@gmail.com>
2024-05-08 22:19:59 -0500

    started project with vite + vue 3

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8080,
+  },
+  preview: {
+    port: 8080,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      },
+    ]
+  },
+  build: {
+    cssCodeSplit: false,
+    minify: true,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        },
+    },
+  },
+})
