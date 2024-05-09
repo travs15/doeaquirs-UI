9ef712f8b10ee77594236d2d056f485b0d180d27
Alejandro Reyes <alejo.reyes.p15@gmail.com>
2024-05-09 02:25:42 -0500

    added home sections and actions, created forms from template

---
+++ src/components/DonationForm.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="registerFormContainer">
+    <h2>Registro</h2>
+    <form>
+        <div class="mb-3">
+            <label for="emailInput" class="form-label">Email Address</label>
+            <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="nameInput" class="form-label">Name</label>
+            <input type="text" class="form-control" id="nameInput" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="lastNameInput" class="form-label">Last Name</label>
+            <input type="text" class="form-control" id="lastNameInput" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="phoneInput" class="form-label">Phone Number</label>
+            <input type="tel" class="form-control" id="phoneInput" placeholder="555-5555555">
+        </div>
+        <div class="mb-3">
+            <label for="passwordInput" class="form-label">Password</label>
+            <input type="password" class="form-control" id="passwordInput" placeholder="******">
+        </div>
+        <div class="mb-3">
+            <label for="passwordConfirmInput" class="form-label">Password Confirmation</label>
+            <input type="password" class="form-control" id="passwordConfirmInput" placeholder="******">
+        </div>
+        <div class="buttonsContainer">
+            <button type="button" class="btn btn-secondary" @click="register">
+                Register
+            </button type="button">
+            <button type="button" class="btn btn-secondary" @click="login">
+                 Go to Log-In
+            </button type="button">
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            name: '',
+            lastName: '',
+            phoneNumber: '',
+            password: '',
+            passwordConfirm: '',
+        }
+    },
+    methods: {
+        register() {
+            // TODO Implement registration functionality here
+            this.$router.push('/home');
+        },
+        login() {
+            this.$router.push('/login')
+        },
+    }
+}
+
+
+</script>
+
+<style scoped>
+/* Your light theme styles */
+.registerFormContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;   
+    width: 100%;
+}
+
+form {
+    border: 0.5px solid #ccc;
+    border-radius: .5em;
+    width: 30%;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+    margin: 1em;
+    padding: 1em;
+}
+
+.button {
+    margin: 0.5em;
+    font-weight: 900;
+    width: 40%;
+}
+
+.dateContainer {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+
+.buttonsContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,61 +1,105 @@
 <template>
-    <Modal :show="showUserOptionsModal" @close-modal="closeUserOptionsModal">
-        <UserOptionsModal />
-    </Modal>
-    <button @click=""></button>
+    <div class="homeContainer">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Doeaquirs!</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#" @click="handleSignOut">Log-Out</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="buttonsContainer container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="sectionContainer">
+                        <button type="button" class="btn btn-outline-success" @click="handlePostDonation">
+                            <h3>Post Donation!</h3>
+                        </button type="button">
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="sectionContainer">
+                        <button type="button" class="btn btn-outline-primary" @click="handlePostDrive">
+                            <h3>Post Drive!</h3>
+                        </button type="button">
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="sectionContainer">
+                        <div class="alert alert-warning" role="alert">
+                            Support Done!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
-<script setup>
-// with the setup tag we don't need to expose the variables or methods to the template by exporting them
-import { onMounted, ref} from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios';
-import Modal from '@/components/Modal.vue';
-import UserOptionsModal from '@/components/UserOptionsModal.vue';
-//service
-import useApiService from '@/services/apiService';
-
-/** Global variables */
-const router = useRouter();
-const apiService = useApiService();
-
-/** Log in state and Log out User */
-const isLoggedIn = ref(false);
-let auth;
-
-const data = {
-  username: "bahs",
-  name: "Boris",
-  lastName: "Hurtado",
-  email: "boris@test.com",
-  birthDate: "03-11-1993",
-  password: "123456789",
-};
-
-// hook lifecycle
-onMounted(() => {
-  apiService.post('api/v1/auth/register', data)
-      .then(response => console.log(response))
-      .catch(error => console.log(error))
-});
-
-const handleSignOut = () => {
-  router.push('/login');
-};
-
-/** Modal Logic */
-const showUserOptionsModal = ref(false);
-const openUserOptionsModal = () => {
-  showUserOptionsModal.value = true;
-};
-const closeUserOptionsModal = () => {
-  showUserOptionsModal.value = false;
-};
-
-
+<script>
+export default {
+    data() {
+        return {
+            // email: '',
+            // password: '',
+            isLoggedIn: false,
+            showUserOptionsModal: false,
+        }
+    },
+    methods: {
+        openUserOptionsModal() {
+            showUserOptionsModal.value = true;
+        },
+        closeUserOptionsModal() {
+            showUserOptionsModal.value = false;
+        },
+        handleSignOut() {
+            router.push('/login');
+        },
+        handlePostDonation() {
+            // TODO show modal with form
+        },
+        handlePostDrive() {
+            // TODO show modal with form
+        }
+    }
+}
 </script>
 
 <style scoped>
+.homeContainer {
+    width: 100%;
+    height: 100vh;
+}
 
+.buttonsContainer {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+}
+
+.sectionContainer {
+    border-radius: 3em;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+    margin: 1em;
+    padding: 1em;
+    height: 40vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    overflow-y: scroll;
+}
 </style>
