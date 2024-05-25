099bd8432767db3fdca868b1ac206cbe2572fc2c
Alejandro Reyes <alejo.reyes.p15@gmail.com>
2024-05-25 11:59:30 -0500

    styled register view to new design

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,121 +1,110 @@
 <template>
     <div class="registerFormContainer">
         <h2>Register</h2>
-        <div class="container-fluid">
-            <div class="row justify-content-md-center">
-                <div class="col-12 col-sm-10 col-md-6">
-                    <form>
-                        <div class="mb-3">
-                            <!-- <label for="emailInput" class="form-label">Email Address</label> -->
-                            <input
-                                type="email" class="form-control" id="emailInput"
-                                placeholder="name@example.com"
-                                v-model.trim="v$.newUser.email.$model"
-                            >
-                            <div class="validationMessages">
-                                <span
-                                    class="validationError"
-                                    v-show="v$.newUser.email.required.$invalid && v$.newUser.email.$dirty">
-                                    Enter an email
-                                </span>
-                                <span
-                                    class="validationError"
-                                    v-show="v$.newUser.email.email.$invalid && v$.newUser.email.$dirty">
-                                    Enter a valid email
-                                </span>
-                            </div>
+        <form>
+            <div class="register">
+                <div class="register-box">
+                    <div class="register-line">
+                        <div class="register-line-label">Email</div>
+                        <div class="register-line-input">
+                            <input type="email" v-model.trim="v$.newUser.email.$model" placeholder="Digite sua email">
                         </div>
-                        <div class="mb-3">
-                            <!-- <label for="nameInput" class="form-label">Name</label> -->
-                            <input
-                                type="text" class="form-control" id="nameInput"
-                                placeholder="Name"
-                                v-model.trim="v$.newUser.name.$model"
-                            >
-                            <div class="validationMessages">
-                                <span
-                                    class="validationError"
-                                    v-show="v$.newUser.name.required.$invalid && v$.newUser.name.$dirty">
-                                    Enter your name
-                                </span>
-                            </div>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span
+                            class="register-validationError"
+                            v-show="v$.newUser.email.required.$invalid && v$.newUser.email.$dirty">
+                            Enter an email
+                        </span>
+                        <span
+                            class="register-validationError"
+                            v-show="v$.newUser.email.email.$invalid && v$.newUser.email.$dirty">
+                            Enter a valid email
+                        </span>
+                    </div>
+                    <div class="register-line">
+                        <div class="register-line-label">Name</div>
+                        <div class="register-line-input">
+                            <input type="text" v-model.trim="v$.newUser.name.$model" placeholder="Name">
                         </div>
-                        <div class="mb-3">
-                            <!-- <label for="lastNameInput" class="form-label">Last Name</label> -->
-                            <input
-                                type="text" class="form-control" id="lastNameInput"
-                                placeholder="Last Name"
-                                v-model.trim="v$.newUser.lastName.$model"
-                            >
-                            <div class="validationMessages">
-                                <span class="validationError"
-                                    v-show="v$.newUser.lastName.required.$invalid && v$.newUser.lastName.$dirty">
-                                    Enter your last name
-                                </span>
-                            </div>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span
+                            class="register-validationError"
+                            v-show="v$.newUser.name.required.$invalid && v$.newUser.name.$dirty">
+                            Enter your name
+                        </span>
+                    </div>
+                    <div class="register-line">
+                        <div class="register-line-label">Last Name</div>
+                        <div class="register-line-input">
+                            <input type="text" v-model.trim="v$.newUser.lastName.$model" placeholder="Last name">
                         </div>
-                        <div class="mb-3">
-                            <!-- <label for="phoneInput" class="form-label">Phone Number</label> -->
-                            <input type="tel" class="form-control" id="phoneInput"
-                                placeholder="555-5555555"
-                                v-model="v$.newUser.phoneNumber.$model"
-                            >
-                            <div class="validationMessages">
-                                <span class="validationError"
-                                    v-show="v$.newUser.phoneNumber.required.$invalid && v$.newUser.phoneNumber.$dirty">
-                                    Enter a phone number
-                                </span>
-                            </div>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span class="register-validationError"
+                            v-show="v$.newUser.lastName.required.$invalid && v$.newUser.lastName.$dirty">
+                            Enter your last name
+                        </span>
+                    </div>
+                    <div class="register-line">
+                        <div class="register-line-label">Phone Number</div>
+                        <div class="register-line-input">
+                            <input type="tel" v-model="v$.newUser.phoneNumber.$model" placeholder="55555555">
                         </div>
-                        <div class="mb-3">
-                            <label for="passwordInput" class="form-label">Password</label>
-                            <input
-                                type="password" class="form-control" id="passwordInput"
-                                placeholder="******"
-                                v-model="v$.newUser.password.$model"
-                                >
-                            <div class="validationMessages">
-                                <span class="validationError"
-                                    v-show="v$.newUser.password.required.$invalid && v$.newUser.password.$dirty">
-                                    Enter a password
-                                </span>
-                                <span class="validationError"
-                                    v-show="v$.newUser.password.minLength.$invalid && v$.newUser.password.$dirty">
-                                    Enter a password with a minimum of 6 characters
-                                </span>
-                            </div>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span class="register-validationError"
+                            v-show="v$.newUser.phoneNumber.required.$invalid && v$.newUser.phoneNumber.$dirty">
+                            Enter a phone number
+                        </span>
+                    </div>
+                    <div class="register-line">
+                        <div class="register-line-label">Password</div>
+                        <div class="register-line-input">
+                            <input type="password" v-model="v$.newUser.password.$model" placeholder="Enter a password">
                         </div>
-                        <div class="mb-3">
-                            <label for="passwordConfirmInput" class="form-label">Password Confirmation</label>
-                            <input
-                                type="password" class="form-control" id="passwordConfirmInput"
-                                placeholder="******"
-                                v-model="v$.newUser.passwordConfirm.$model"
-                            >
-                            <div class="validationMessages">
-                                <span class="validationError"
-                                    v-show="v$.newUser.passwordConfirm.required.$invalid && v$.newUser.passwordConfirm.$dirty">
-                                    Enter the password confirmation
-                                </span>
-                                <span class="validationError"
-                                    v-show="v$.newUser.passwordConfirm.sameAsPassword.$invalid && v$.newUser.passwordConfirm.$dirty">
-                                    The passwords don't match
-                                </span>
-                            </div>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span class="register-validationError"
+                            v-show="v$.newUser.password.required.$invalid && v$.newUser.password.$dirty">
+                            Enter a password
+                        </span>
+                        <span class="register-validationError"
+                            v-show="v$.newUser.password.minLength.$invalid && v$.newUser.password.$dirty">
+                            Enter a password with a minimum of 6 characters
+                        </span>
+                    </div>
+                    <div class="register-line">
+                        <div class="register-line-label">Password Confirmation</div>
+                        <div class="register-line-input">
+                            <input type="password" v-model="v$.newUser.passwordConfirm.$model" placeholder="Confirm your password">
                         </div>
-                        <div class="buttonsContainer">
-                            <button type="button" class="btn btn-secondary" @click="register"
-                                :disabled="this.isDisabled">
-                                Register
-                            </button type="button">
-                            <button type="button" class="btn btn-secondary" @click="login">
-                                Go to Log-In
-                            </button type="button">
-                        </div>
-                    </form>
+                    </div>
+                    <div class="register-validationMessages">
+                        <span class="register-validationError"
+                            v-show="v$.newUser.passwordConfirm.required.$invalid && v$.newUser.passwordConfirm.$dirty">
+                            Enter the password confirmation
+                        </span>
+                        <span class="register-validationError"
+                            v-show="v$.newUser.passwordConfirm.sameAsPassword.$invalid && v$.newUser.passwordConfirm.$dirty">
+                            The passwords don't match
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="btn-container">
+                <div type="button" class="btn btn-primary" @click="register"
+                    :disabled="this.isDisabled">
+                    Register
+                </div>
+            </div>
+            <div class="btn-container">
+                <div type="button" class="btn btn-primary" @click="login">
+                    Go to Log-In
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
@@ -164,7 +153,7 @@ export default {
                     console.log(response.status);
                     // TODO add feedback to user
                     if(response.status === 200) {
-                        this.$router.push('/login');
+                        this.$router.push('/register');
                     }
                 });
         },
@@ -189,13 +178,13 @@ export default {
 
 </script>
 
-<style scoped>
+<style lang='scss' scoped>
 .registerFormContainer {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    /* height: 100%;    */
+    height: 100%;
     width: 100%;
 }
 
@@ -206,30 +195,98 @@ export default {
 }
 
 form {
-    border: 0.5px solid #ccc;
-    border-radius: 1em;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+    background-color: $highlight-background;
+    box-shadow: $shadow;
+    border-radius: 20px;
+    border: $box-border;
+    width: 40%;
     margin: 1em;
     padding: 1em;
 }
 
-.buttonsContainer {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    align-items: center;
+.btn-container {
+    padding: 0.3em;
 }
 
-.validationMessages {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-items: center
-}
+.register {
+    margin-bottom: 1rem;
 
-.validationError {
-    margin-top: 10px;
-    color: rgb(165, 35, 35);
-    font-size: 12px;
+    .register-box {
+        background-color: $highlight-background;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    .register-box input[type=text],
+    .register-box input[type=password],
+    .register-box input[type=email],
+    .register-box input[type=tel],
+    .register-box select {
+        background-color: #FFFFFF;
+        border: 0px;
+        color: $text-color;
+
+        &:focus {
+        outline: none;
+        }
+    }
+
+    .register-line {
+        height: 3rem;
+        border-bottom: $box-border;
+        margin-left: 20px;
+        margin-right: 20px;
+        display: grid;
+        grid-template-columns: 90px auto;
+
+        &:last-of-type {
+            border-bottom: 0px;
+        }
+    }
+
+    .register-line-label {
+        margin-right: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+    }
+
+    .register-line-input {
+        display: flex;
+        align-items: center;
+
+        input[type=text] {
+            padding-left: 4px;
+        }
+
+        input[type=text],
+        select {
+            width: 100%;
+            height: 100%;
+            font-size: 1rem;
+            border-radius: 0;
+            font-family: "Red Hat Display", Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+        
+        }
+
+        input[type=date] {
+            text-align: left;
+        }
+    }
+
+    .register-validationMessages {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-items: flex-end;
+    }
+
+    .register-validationError {
+        margin-left: 20px;
+        margin-right: 20px;
+        margin-top: 10px;
+        color: rgb(165, 35, 35);
+        font-size: 12px;
+    }
 }
 </style>
